40b51ea6e28ca2ab17f618d48ca35550e7f69d7c
john.welsh <john.welsh@gmail.com>
2014-03-30 03:38:36 -0700

    Started work, integrated devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,7 @@ Menu::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  # Default devise setup
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
 end
