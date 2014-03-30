ce5a2841522c4241b6070728c65b51e89ced9532
john.welsh <john.welsh@gmail.com>
2014-03-30 04:40:18 -0700

    fixed secret key env link

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,5 +9,5 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-secret = Rails.env.production? ? ENV['SECRET_TOKEN'] : '91b1778a635c22d870fb7454221d827afa3214f0267a5cc65ba49b0fbd9fe99865acbf29544933ba65776c9e189a54b8b1132cdd1f43bce46df121c6cfe3a027'
+secret = Rails.env.production? ? ENV['SECRET_KEY_BASE'] : '91b1778a635c22d870fb7454221d827afa3214f0267a5cc65ba49b0fbd9fe99865acbf29544933ba65776c9e189a54b8b1132cdd1f43bce46df121c6cfe3a027'
 Menu::Application.config.secret_key_base = secret
